ed85e063c73eedac1af26120d0b8e27271ab15db
ehrickkk <150636585+ehrickkk@users.noreply.github.com>
2026-09-24 13:14:09 +0800

    Add initial intro files for HTML, Python, and JSX
    Uploaded three introductory files to demonstrate initial project setup for Task 3:
    - index.html: HTML file displaying introductory message
    - script.py: Python script printing introductory message
    - App.jsx: React JSX component rendering introductory heading

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>hello this is an introduction to github using javascript</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ script.py
@@ -0,0 +1,1 @@
+print("hello this is an introduction to github using python")
